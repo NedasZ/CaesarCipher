b7d42f1a5d6f824116af4f359372128b92120309
NedasZ <zilovas79@gmail.com>
2020-10-27 10:46:16 +0200

    InitialUpload

---
+++ CaesarCipher/CaesarCipher/CSCipher.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CaesarCipher
+{
+    public class CSCipher
+    {
+        public static char Cipher(char ch, int key)
+        {
+            
+            if (!((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')))
+            {
+                return ch;
+            }
+
+            key = key % 26;
+            if(key < 0)
+                key += 26;
+
+            char d;
+            if (char.IsUpper(ch))
+            {
+                d = 'A';
+            }
+            else
+            {
+                d = 'a';
+            }
+
+            char ats = (char)((((ch + key) - d) % 26) + d);
+
+            return ats;
+        }
+
+        public static string Encipher(string input, int key)
+        {
+            string output = "";
+            
+            foreach (char ch in input)
+                output += Cipher(ch, key);
+
+            return output;
+        }
+
+        public static string Decipher(string input, int key)
+        {
+            return Encipher(input, 26 - key);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("type a message to encrypt it.");
+            string message = Console.ReadLine();
+            bool nokey = true;
+            int key = 0;
+            while (nokey)
+            {
+                Console.WriteLine("type a key to encrypt by (integer number)");
+                string keyString = Console.ReadLine();
+                nokey = false;
+                try
+                {
+                    key = int.Parse(keyString);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Key can not be parsed, try again.");
+                    nokey = true;
+                }
+            }
+
+
+            string secretMessage = Encipher(message, key);
+            Console.WriteLine("Encrypted message: " + secretMessage);
+
+
+            Console.WriteLine("type a message to decrypt it.");
+            string message2 = Console.ReadLine();
+
+
+
+            int key2 = 0;
+
+            nokey = true;
+            while (nokey)
+            {
+                Console.WriteLine("type a key it was encrypted by (integer number).");
+                string keyString = Console.ReadLine();
+                nokey = false;
+                try
+                {
+                    key2 = int.Parse(keyString);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Key can not be parsed, try again.");
+                    nokey = true;
+                }
+            }
+
+            string decryptedMessage = Decipher(message2, key2);
+            Console.WriteLine("Decrypted message: " + decryptedMessage);
+
+            Console.WriteLine("(Press 'Enter' to close program)");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ CaesarCipher/CaesarCipherTests/CSCipherTests.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CaesarCipher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarCipher.Tests
+{
+    [TestClass()]
+    public class CSCipherTests
+    {
+        [TestMethod()]
+        public void CipherTestUpperLetter()
+        {
+            char upperLetter = 'A';
+            int key = 1;
+            char rezult = CSCipher.Cipher(upperLetter, key);
+            Assert.AreEqual('B', rezult);
+        }
+
+        [TestMethod()]
+        public void CipherTestLowerLetter()
+        {
+            char upperLetter = 'a';
+            int key = 1;
+            char rezult = CSCipher.Cipher(upperLetter, key);
+            Assert.AreEqual('b', rezult);
+        }
+
+        [TestMethod()]
+        public void CipherTestNonLetter()
+        {
+            char upperLetter = '5';
+            int key = 1;
+            char rezult = CSCipher.Cipher(upperLetter, key);
+            Assert.AreEqual('5', rezult);
+        }
+
+        [TestMethod()]
+        public void CipherTestLargeKey()
+        {
+            char upperLetter = 'A';
+            int key = 53;
+            char rezult = CSCipher.Cipher(upperLetter, key);
+            Assert.AreEqual('B', rezult);
+        }
+
+        [TestMethod()]
+        public void CipherTestNegativeKey()
+        {
+            char upperLetter = 'A';
+            int key = -1;
+            char rezult = CSCipher.Cipher(upperLetter, key);
+            Assert.AreEqual('Z', rezult);
+        }
+
+        [TestMethod()]
+        public void CipherTestLargeNegativeKey()
+        {
+            char upperLetter = 'A';
+            int key = -1015;
+            char rezult = CSCipher.Cipher(upperLetter, key);
+            Assert.AreEqual('Z', rezult);
+        }
+        [TestMethod()]
+        public void CipherTestNonEnglishLetter()
+        {
+            char upperLetter = 'Ą';
+            int key = 1;
+            char rezult = CSCipher.Cipher(upperLetter, key);
+            Assert.AreEqual('Ą', rezult);
+        }
+
+        [TestMethod()]
+        public void EncipherTestBasic()
+        {
+            string message = "abc";
+            int key = 3;
+            string rezult = CSCipher.Encipher(message, key);
+            Assert.AreEqual("def", rezult);
+        }
+
+        [TestMethod()]
+        public void EncipherTestWithAdditionalSymbols()
+        {
+            string message = "a b;c1";
+            int key = 3;
+            string rezult = CSCipher.Encipher(message, key);
+            Assert.AreEqual("d e;f1", rezult);
+        }
+
+        [TestMethod()]
+        public void DecipherTestBasic()
+        {
+            string message = "def";
+            int key = 3;
+            string rezult = CSCipher.Decipher(message, key);
+            Assert.AreEqual("abc", rezult);
+        }
+
+        [TestMethod()]
+        public void DecipherTestWithAdditionalSymbols()
+        {
+            string message = "d e;f1";
+            int key = 3;
+            string rezult = CSCipher.Decipher(message, key);
+            Assert.AreEqual("a b;c1", rezult);
+        }
+    }
+}
